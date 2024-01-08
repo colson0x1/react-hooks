605319098ce07b4282d709fe6a3ec60c91621512
Colson <colsoncalifornication@gmail.com>
2024-01-08 11:33:13 +0545

    build a custom hook: useToggleState

--- src/Toggler.jsx
+++ src/Toggler.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
+import useToggle from './hooks/useToggle';
 
 function Toggler() {
+  /*
+   * No needed after useToggler hooks is used!
   const [isHappy, setIsHappy] = useState(true);
   const [isHeartbroken, setIsHeartbroken] = useState(false);
 
@@ -11,11 +14,19 @@ function Toggler() {
   const toggleIsBroken = () => {
     setIsHeartbroken(!isHeartbroken);
   };
+  */
+
+  // Using useToggler hook
+  // WE capture the two things we get back from array
+  // 1. The actual piece of state
+  // 2. & Function to toggle that piece of state
+  const [isHappy, toggleIsHappy] = useToggle(true);
+  const [isHeartbroken, toggleIsHeartbroken] = useToggle(false);
 
   return (
     <div>
-      <h1 onClick={toggleIsHapy}>{isHappy ? '😄' : '😢'}</h1>
-      <h1 onClick={toggleIsBroken}>{isHeartbroken ? '💔' : '❤️ '}</h1>
+      <h1 onClick={toggleIsHappy}>{isHappy ? '😄' : '😢'}</h1>
+      <h1 onClick={toggleIsHeartbroken}>{isHeartbroken ? '💔' : '❤️ '}</h1>
     </div>
   );
 }
---
+++ src/hooks/useToggle.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+function useToggle(initialVal = false) {
+  // call useState, 'reserve piece of state'
+  const [state, setState] = useState(initialVal);
+
+  const toggle = () => {
+    setState(!state);
+  };
+
+  return [state, toggle];
+}
+
+export default useToggle;
