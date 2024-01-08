7a0197777a347bb5fc90273804018897b0a2ce71
Colson <colsoncalifornication@gmail.com>
2024-01-08 10:31:21 +0545

    implement counter with classic class component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import CounterClass from './CounterClass';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <CounterClass />
+      </div>
+    );
+  }
+}
+
+export default App;
